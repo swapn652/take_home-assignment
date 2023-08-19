b42f3f1cb4641aed1da4195c3338cfdd5481677a
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-08-19 18:20:40 +0530

    Created basic draft of application

--- locate-me/popup.tsx
+++ locate-me/popup.tsx
@@ -1,15 +1,27 @@
 import { useState } from "react"
+import { fetchCountryAndCity, fetchIPAddress } from "~api/api";
 
 function IndexPopup() {
   const [ipAddress, setIpAddress] = useState<string | null>(null);
   const [city, setCity] = useState<string | null>(null);
   const [country, setCountry] = useState<string | null>(null);
 
+  const handleClick = async () => {
+    const ip = await fetchIPAddress();
+    setIpAddress(ip);
+
+    const countryCityData = await fetchCountryAndCity(ip);
+    setCity(countryCityData.city);
+    setCountry(countryCityData.country);
+  }
+
   return (
-    <div>
-      <button>
+    <div style = {{width: 500, height: 500}}>
+      <button onClick={handleClick}>
         Show My Location
       </button>   
+      {country && <div>Your country is {country}</div>}
+      {city && <div>and your city is {city}</div>}
     </div>
   )
 }
