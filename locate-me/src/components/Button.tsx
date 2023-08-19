3d5ecc50249912345720a680dd2b9046c00379e7
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-08-19 18:42:26 +0530

    Added loading functionality

--- locate-me/src/components/Button.tsx
+++ locate-me/src/components/Button.tsx
@@ -1,24 +1,36 @@
 import React from 'react'
 import { useState } from "react"
+import ClipLoader from "react-spinners/ClipLoader";
 import { fetchCountryAndCity, fetchIPAddress } from "~api/api";
 
 export const Button = () => {
     const [ipAddress, setIpAddress] = useState<string | null>(null);
     const [city, setCity] = useState<string | null>(null);
     const [country, setCountry] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
   
     const handleClick = async () => {
       const ip = await fetchIPAddress();
       setIpAddress(ip);
+      setLoading(true);
   
       const countryCityData = await fetchCountryAndCity(ip);
       setCity(countryCityData.city);
       setCountry(countryCityData.country);
+      setLoading(false);
     }
 
   return (
     <div className='h-72 w-60'>
-        <button
+        <ClipLoader
+            color="#ffffff"
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+            className='ml-10'
+        />
+        {!loading && <button
             className="
             bg-white 
             text-black 
@@ -33,6 +45,7 @@ export const Button = () => {
         >
             Show My Location
           </button>
+        }
 
           <div className='w-[300px] mt-10 ml-4'>
             {country && <div className="text-2xl text-white">Your country is {country == 'IN' ? `India` : 'None'}</div>}
