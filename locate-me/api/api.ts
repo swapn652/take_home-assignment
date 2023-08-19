c6a52946ceba96b7cc7d2fcff598fa5ffe355f0f
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-08-19 18:13:25 +0530

    Created functions to fetch data

---
+++ locate-me/api/api.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import type { AxiosResponse } from "axios";
+
+interface IpifyResponse {
+  ip: string;
+}
+
+interface IpinfoResponse {
+    city: string;
+    country: string;
+  }
+
+export async function fetchIPAddress(): Promise<string> {
+  try {
+    const response: AxiosResponse<IpifyResponse> = await axios.get("https://api.ipify.org?format=json");
+    console.log(process.env.PLASMO_PUBLIC_IPINFO_TOKEN)
+    return response.data.ip;
+  } catch (error) {
+    throw new Error("Error fetching IP address: " + error.message);
+  }
+}
+
+export async function fetchCountryAndCity(ipAddress: string): Promise<IpinfoResponse> {
+    try {
+      const response: AxiosResponse<IpinfoResponse> = await axios.get(`https://ipinfo.io/${ipAddress}?token=${process.env.PLASMO_PUBLIC_IPINFO_TOKEN }`);
+      return response.data;
+    } catch (error) {
+      throw new Error("Error fetching country and city: " + error.message);
+    }
+}
+
+
+
--- locate-me/popup.tsx
+++ locate-me/popup.tsx
@@ -1,27 +1,15 @@
 import { useState } from "react"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [ipAddress, setIpAddress] = useState<string | null>(null);
+  const [city, setCity] = useState<string | null>(null);
+  const [country, setCountry] = useState<string | null>(null);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div>
+      <button>
+        Show My Location
+      </button>   
     </div>
   )
 }
